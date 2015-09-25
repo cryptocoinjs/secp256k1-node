7ee5b884059a251b2a347ada9d498c4c0644f5bb
wanderer <mjbecze@gmail.com>
2015-09-25 19:47:48 +0000

    added test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-//tests against the `ecdsa` module
+// tests against the `ecdsa` module
 var ecdsa = require('ecdsa')
 var BigInteger = require('bigi')
 var assert = require('assert')
@@ -14,7 +14,6 @@ var pubKey
 var compactSig
 
 describe('it should handle basic ecdsa ops', function () {
-
   it('should create a public key', function () {
     ck = new CoinKey(privateKey, true)
     pubKey = ecdsaNative.createPublicKey(privateKey, true)
@@ -36,14 +35,14 @@ describe('it should handle basic ecdsa ops', function () {
   })
 
   it('should verify a signature', function () {
-    //testing verification
+    // testing verification
     var sig2 = ecdsa.sign(msg, ck.privateKey)
     sig2 = new Buffer(ecdsa.serializeSig(sig2))
     assert(ecdsaNative.verify(msg, sig2, pubKey), 'should verify signature')
   })
 
   it('should NOT verify an invalid signature', function () {
-    //testing verification
+    // testing verification
     var sig2 = ecdsa.sign(msg, ck.privateKey)
     sig2 = new Buffer(ecdsa.serializeSig(sig2))
     sig2[0] = 0xff
@@ -51,7 +50,7 @@ describe('it should handle basic ecdsa ops', function () {
   })
 
   it('should verify a signature async', function (done) {
-    //testing verification
+    // testing verification
     var sig2 = ecdsa.sign(msg, ck.privateKey)
     sig2 = new Buffer(ecdsa.serializeSig(sig2))
     var r = ecdsaNative.verify(msg, sig2, pubKey)
@@ -63,7 +62,7 @@ describe('it should handle basic ecdsa ops', function () {
 
   it('should create a compact signature', function () {
     var sig = ecdsaNative.sign(msg, privateKey)
-      //save to use to test verifyCompact
+    // save to use to test verifyCompact
     compactSig = sig
 
     var s = {
@@ -77,8 +76,8 @@ describe('it should handle basic ecdsa ops', function () {
     assert(key.getEncoded().toString('hex') === pubKey.toString('hex'), 'the recovered Key should be the same as the public key')
   })
 
-  it('should create a compact signature async', function(done) {
-    ecdsaNative.sign(msg, privateKey, function(err, sig) {
+  it('should create a compact signature async', function (done) {
+    ecdsaNative.sign(msg, privateKey, function (err, sig) {
       var s = {
           r: BigInteger.fromBuffer(sig.signature.slice(0, 32)),
           s: BigInteger.fromBuffer(sig.signature.slice(32, 64)),
@@ -92,59 +91,64 @@ describe('it should handle basic ecdsa ops', function () {
     })
   })
 
-  it('should recover a compact signature and return the public key', function() {
+  it('should recover a compact signature and return the public key', function () {
     var sig = ecdsaNative.recover(msg, compactSig)
     assert(sig.toString('hex') === pubKey.toString('hex'))
   })
 
-  it('should recover a compact signature and return the public key, async', function(done) {
-    ecdsaNative.recover(msg, compactSig, function(result, sig) {
+  it('should recover a compact signature and return the public key, async', function (done) {
+    ecdsaNative.recover(msg, compactSig, function (result, sig) {
       assert(sig.toString('hex') === pubKey.toString('hex'))
       done()
     })
   })
 })
 
-
-describe('invalid inputs', function() {
-  it('should not crash when recoverId is out of bounds - sync', function() {
-    assert.throws(function(){
+describe('invalid inputs', function () {
+  it('should not crash when recoverId is out of bounds - sync', function () {
+    assert.throws(function () {
       var sig = ecdsaNative.recover(msg, compactSig.signature, -27, true)
     })
   })
 
-  it('should not crash when recoverId is out of bounds - async', function() {
-    assert.throws(function(){
-      ecdsaNative.recover(msg, compactSig.signature, -27, true, function(err, sig) {
+  it('should not crash when recoverId is out of bounds - async', function () {
+    assert.throws(function () {
+      ecdsaNative.recover(msg, compactSig.signature, -27, true, function (err, sig) {
         assert(err)
         assert.strictEqual(sig, undefined)
       })
     })
   })
 
-  it('should not crash when giving it an undefined private key', function(done) {
-    try{
+  it('should not crash when giving it an undefined private key', function (done) {
+    try {
       ecdsaNative.sign(undefined, new Buffer('test'))
-    }catch(e){
+    } catch(e) {
       done()
     }
   })
 
-
-  it('should not crash when given an undefined privateKey in createPublicKey', function(done){
-    try{
+  it('should not crash when given an undefined privateKey in createPublicKey', function (done) {
+    try {
       ecdsaNative.createPublicKey()
-    }catch(e){
+    } catch(e) {
       done()
     }
   })
 
-  it('should not crash when given an undefined publickeys in verify', function(done){
-    try{
+  it('should not crash when given an undefined publickeys in verify', function (done) {
+    try {
       ecdsaNative.verify(msg, false)
-    }catch(e){
+    } catch(e) {
       done()
     }
   })
 
+  it('should not crash when sign tx with undefined', function (done) {
+    try {
+      ecdsaNative.sign(null, null)
+    } catch(e) {
+      done()
+    }
+  })
 })
