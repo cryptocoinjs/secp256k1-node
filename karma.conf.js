3f4c8a0adfb09c96fd0d2a163c654e642f6bdcde
Kirill Fomichev <fanatid@ya.ru>
2015-10-30 09:58:43 +0300

    add karma & eslint

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['browserify', 'detectBrowsers', 'mocha'],
+    files: [
+      'test/*.js'
+    ],
+    preprocessors: {
+      'test/*.js': ['browserify']
+    },
+    singleRun: true,
+    plugins: [
+      'karma-browserify',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-detect-browsers',
+      'karma-mocha'
+    ],
+    browserify: {
+      debug: true
+    },
+    detectBrowsers: {
+      enabled: true,
+      usePhantomJS: false,
+      postDetection: function (availableBrowser) {
+        if (process.env.TRAVIS) {
+          return ['Firefox']
+        }
+
+        var browsers = ['Chrome', 'Firefox']
+        return browsers.filter(function (browser) {
+          return availableBrowser.indexOf(browser) !== -1
+        })
+      }
+    }
+  })
+}
