52cec1539a313877e4f28e3839b660a2bbc4b466
Kirill Fomichev <fanatid@ya.ru>
2015-12-20 18:55:42 +0500

    Add utils/update_messages.js

---
+++ utils/update_messages.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var path = require('path')
+
+var sourceFilename = path.join(__dirname, '../src/messages.h')
+var outputFilename = path.join(__dirname, '../lib/messages.json')
+
+var result = {}
+var messages = fs.readFileSync(sourceFilename, 'utf-8').split('\n')
+for (var i = 0; i < messages.length; ++i) {
+  var match = messages[i].match(/#define ([A-Z_]+) "(.*)"/)
+  if (match !== null) {
+    result[match[1]] = match[2]
+  }
+}
+
+fs.writeFileSync(outputFilename, JSON.stringify(result, null, 2) + '\n')
