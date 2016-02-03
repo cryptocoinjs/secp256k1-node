98f1767726ef884901c1bd447c38762944ae3f07
Kirill Fomichev <fanatid@ya.ru>
2016-02-03 16:35:03 +0500

    Add extra tests for add/sub/etc..

--- test/bn/index.js
+++ test/bn/index.js
@@ -15,6 +15,21 @@ describe('BN', function () {
   })
 
   describe('fromNumber', function () {
+    it('0', function () {
+      var bn = BN.fromNumber(0)
+      bnUtil.testBN(bn, BigNum(0))
+    })
+
+    it('2**26-1', function () {
+      var bn = BN.fromNumber(0x03ffffff)
+      bnUtil.testBN(bn, BigNum(0x03ffffff))
+    })
+
+    it('2**26 should equal 0', function () {
+      var bn = BN.fromNumber(0x04000000)
+      bnUtil.testBN(bn, BigNum(0))
+    })
+
     util.repeatIt('random tests', util.env.repeat, function () {
       var b32 = util.getMessage()
       var num = ((b32[0] << 24) + (b32[1] << 16) + (b32[2] << 8) + b32[3]) & 0x03ffffff
@@ -25,7 +40,7 @@ describe('BN', function () {
 
   describe('fromBuffer/toBuffer', function () {
     for (var i = 0; i < 10; ++i) {
-      it('all bits in #' + i + ' word', function () {
+      it('all bits eq 1 in #' + i + ' word', function () {
         var b32 = bnUtil.fillZeros(BigNum.pow(2, 26).sub(1).shiftLeft(26 * i).toBuffer())
         var bn = BN.fromBuffer(b32)
         bnUtil.testBN(bn, BigNum.fromBuffer(b32))
@@ -38,6 +53,194 @@ describe('BN', function () {
       var bn = BN.fromBuffer(b32)
       bnUtil.testBN(bn, BigNum.fromBuffer(b32))
       expect(bn.toBuffer().toString('hex')).to.equal(b32.toString('hex'))
+    })
+  })
+
+  describe.skip('clone', function () {})
+
+  describe('strip', function () {
+    it('[0] -> [0]', function () {
+      var bn = new BN()
+      bn.words = [0]
+      bn.length = 1
+      bnUtil.testBN(bn.strip(), BigNum(0))
+    })
+
+    it('[1] -> [1]', function () {
+      var bn = new BN()
+      bn.words = [1]
+      bn.length = 1
+      bnUtil.testBN(bn.strip(), BigNum(1))
+    })
+
+    it('[0, 0] -> [0]', function () {
+      var bn = new BN()
+      bn.words = [0, 0]
+      bn.length = 2
+      bnUtil.testBN(bn.strip(), BigNum(0))
+    })
+
+    it('[1, 0] -> [1]', function () {
+      var bn = new BN()
+      bn.words = [1, 0]
+      bn.length = 2
+      bnUtil.testBN(bn.strip(), BigNum(1))
+    })
+  })
+
+  describe('normSign', function () {
+    it('1 -> 1', function () {
+      var bn = BN.fromNumber(1)
+      bnUtil.testBN(bn.normSign(), BigNum(1))
+    })
+
+    it('-1 -> -1', function () {
+      var bn = BN.fromNumber(1)
+      bn.negative = 1
+      bnUtil.testBN(bn.normSign(), BigNum(-1))
+    })
+
+    it('0 -> 0', function () {
+      var bn = BN.fromNumber(0)
+      bnUtil.testBN(bn.normSign(), BigNum(0))
+    })
+
+    it('-0 -> 0', function () {
+      var bn = BN.fromNumber(0)
+      bn.negative = 1
+      bnUtil.testBN(bn.normSign(), BigNum(0))
+    })
+
+    it('0x04000000 -> 0x04000000', function () {
+      var b32 = bnUtil.fillZeros(new Buffer([0xff, 0xff, 0xff, 0xff]))
+      var bn = BN.fromBuffer(b32)
+      bnUtil.testBN(bn.normSign(), BigNum.fromBuffer(b32))
+    })
+
+    it('-0x04000000 -> -0x04000000', function () {
+      var b32 = bnUtil.fillZeros(new Buffer([0xff, 0xff, 0xff, 0xff]))
+      var bn = BN.fromBuffer(b32)
+      bn.negative = 1
+      bnUtil.testBN(bn.normSign(), BigNum.fromBuffer(b32).neg())
+    })
+  })
+
+  describe('isEven', function () {
+    it('[0] -> true', function () {
+      expect(BN.fromNumber(0).isEven()).to.be.true
+    })
+
+    it('[1] -> false', function () {
+      expect(BN.fromNumber(1).isEven()).to.be.false
+    })
+  })
+
+  describe('isOdd', function () {
+    it('[0] -> false', function () {
+      expect(BN.fromNumber(0).isOdd()).to.be.false
+    })
+
+    it('[1] -> true', function () {
+      expect(BN.fromNumber(1).isOdd()).to.be.true
+    })
+  })
+
+  describe('isZero', function () {
+    it('[0] -> true', function () {
+      expect(BN.fromNumber(0).isZero()).to.be.true
+    })
+
+    it('[1] -> false', function () {
+      expect(BN.fromNumber(1).isZero()).to.be.false
+    })
+  })
+
+  describe('ucmp', function () {
+    it('a.length > b.length', function () {
+      var a = new BN()
+      a.length = 2
+      var b = new BN()
+      b.length = 1
+      expect(a.ucmp(b)).to.equal(1)
+    })
+
+    it('a.length < b.length', function () {
+      var a = new BN()
+      a.length = 1
+      var b = new BN()
+      b.length = 2
+      expect(a.ucmp(b)).to.equal(-1)
+    })
+
+    it('[2] > [1]', function () {
+      var a = BN.fromNumber(2)
+      var b = BN.fromNumber(1)
+      expect(a.ucmp(b)).to.equal(1)
+    })
+
+    it('[1] < [2]', function () {
+      var a = BN.fromNumber(1)
+      var b = BN.fromNumber(2)
+      expect(a.ucmp(b)).to.equal(-1)
+    })
+
+    it('[1] = [1]', function () {
+      var a = BN.fromNumber(1)
+      var b = BN.fromNumber(1)
+      expect(a.ucmp(b)).to.equal(0)
+    })
+
+    it('-2 > 1', function () {
+      var a = BN.fromNumber(2)
+      a.negative = 1
+      var b = BN.fromNumber(1)
+      expect(a.ucmp(b)).to.equal(1)
+    })
+
+    it('1 < -2', function () {
+      var a = BN.fromNumber(1)
+      var b = BN.fromNumber(2)
+      b.negative = 1
+      expect(a.ucmp(b)).to.equal(-1)
+    })
+
+    it('-1 = -1', function () {
+      var a = BN.fromNumber(1)
+      a.negative = 1
+      var b = BN.fromNumber(1)
+      b.negative = 1
+      expect(a.ucmp(b)).to.equal(0)
+    })
+
+    util.repeatIt('random tests', util.env.repeat, function () {
+      var b32a = util.getMessage()
+      var b32b = util.getTweak()
+      var a = BN.fromBuffer(b32a)
+      var b = BN.fromBuffer(b32b)
+      expect(a.ucmp(b)).to.equal(BigNum.fromBuffer(b32a).cmp(BigNum.fromBuffer(b32b)))
+    })
+  })
+
+  describe('gtOne', function () {
+    it('0', function () {
+      var bn = BN.fromNumber(0)
+      expect(bn.gtOne()).to.be.false
+    })
+
+    it('1', function () {
+      var bn = BN.fromNumber(1)
+      expect(bn.gtOne()).to.be.false
+    })
+
+    it('2', function () {
+      var bn = BN.fromNumber(2)
+      expect(bn.gtOne()).to.be.true
+    })
+
+    it('length > 1', function () {
+      var bn = new BN()
+      bn.length = 2
+      expect(bn.gtOne()).to.be.true
     })
   })
 
@@ -97,31 +300,136 @@ describe('BN', function () {
     })
   })
 
-  describe('add', function () {
+  describe('bitLengthGT256', function () {
+    it('length eq 9', function () {
+      var bn = new BN()
+      bn.length = 9
+      expect(bn.bitLengthGT256()).to.be.false
+    })
+
+    it('length eq 10 and last word is 0x003fffff', function () {
+      var bn = new BN()
+      bn.words = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0x003fffff]
+      bn.length = 10
+      expect(bn.bitLengthGT256()).to.be.false
+    })
+
+    it('length eq 10 and last word is 0x00400000', function () {
+      var bn = new BN()
+      bn.words = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00400000]
+      bn.length = 10
+      expect(bn.bitLengthGT256()).to.be.true
+    })
+
+    it('length eq 11', function () {
+      var bn = new BN()
+      bn.length = 11
+      expect(bn.bitLengthGT256()).to.be.true
+    })
+  })
+
+  describe('iuaddn', function () {
+    it('1 + 1', function () {
+      var bn = BN.fromNumber(1)
+      bnUtil.testBN(bn.iuaddn(1), BigNum(2))
+    })
+
+    it('0x03ffffff + 1', function () {
+      var bn = BN.fromNumber(0x03ffffff)
+      bnUtil.testBN(bn.iuaddn(1), BigNum(0x04000000))
+    })
+
+    util.repeatIt('random tests', util.env.repeat, function () {
+      var b32 = util.getMessage()
+      b32[0] = 0
+      var bn = BN.fromBuffer(b32)
+      var x = ((b32[1] << 24) + (b32[2] << 16) + (b32[3] << 8) + b32[4]) & 0x03ffffff
+      bnUtil.testBN(bn.iuaddn(x), BigNum.fromBuffer(b32).add(x))
+    })
+  })
+
+  describe('iadd', function () {
     util.repeatIt('random tests', util.env.repeat, function () {
       var b32A = util.getMessage()
       var b32B = util.getTweak()
-      var bnR = BigNum.fromBuffer(b32A).add(BigNum.fromBuffer(b32B))
 
       var a = BN.fromBuffer(b32A)
+      var ac = a.clone()
       var b = BN.fromBuffer(b32B)
-      var r = a.add(b)
 
-      bnUtil.testBN(r, bnR)
+      // a + b
+      bnUtil.testBN(a.iadd(b), BigNum.fromBuffer(b32A).add(BigNum.fromBuffer(b32B)))
+      a.words = ac.clone().words
+      a.length = ac.length
+      a.negative = 1
+      // (-a) + b
+      bnUtil.testBN(a.iadd(b), BigNum.fromBuffer(b32A).neg().add(BigNum.fromBuffer(b32B)))
+      a.words = ac.clone().words
+      a.length = ac.length
+      a.negative = 1
+      b.negative = 1
+      // (-a) + (-b)
+      bnUtil.testBN(a.iadd(b), BigNum.fromBuffer(b32A).neg().add(BigNum.fromBuffer(b32B).neg()))
+      a.words = ac.clone().words
+      a.length = ac.length
+      a.negative = 0
+      // a + (-b)
+      bnUtil.testBN(a.iadd(b), BigNum.fromBuffer(b32A).add(BigNum.fromBuffer(b32B).neg()))
+    })
+  })
+
+  describe('add', function () {
+    it('source was not affected', function () {
+      var b32a = util.getMessage()
+      var b32b = util.getTweak()
+
+      var a = BN.fromBuffer(b32a)
+      var b = BN.fromBuffer(b32b)
+
+      bnUtil.testBN(a.add(b), BigNum.fromBuffer(b32a).add(BigNum.fromBuffer(b32b)))
+      expect(a.toBuffer().toString('hex')).to.equal(b32a.toString('hex'))
+    })
+  })
+
+  describe('isub', function () {
+    util.repeatIt('random tests', util.env.repeat, function () {
+      var b32A = util.getMessage()
+      var b32B = util.getTweak()
+
+      var a = BN.fromBuffer(b32A)
+      var ac = a.clone()
+      var b = BN.fromBuffer(b32B)
+
+      // a - b
+      bnUtil.testBN(a.isub(b), BigNum.fromBuffer(b32A).sub(BigNum.fromBuffer(b32B)))
+      a.words = ac.clone().words
+      a.length = ac.length
+      a.negative = 1
+      // (-a) + b
+      bnUtil.testBN(a.isub(b), BigNum.fromBuffer(b32A).neg().sub(BigNum.fromBuffer(b32B)))
+      a.words = ac.clone().words
+      b.negative = 1
+      a.length = ac.length
+      // (-a) + (-b)
+      bnUtil.testBN(a.isub(b), BigNum.fromBuffer(b32A).neg().sub(BigNum.fromBuffer(b32B).neg()))
+      a.words = ac.clone().words
+      a.negative = 0
+      // a + (-b)
+      a.length = ac.length
+      bnUtil.testBN(a.isub(b), BigNum.fromBuffer(b32A).sub(BigNum.fromBuffer(b32B).neg()))
     })
   })
 
   describe('sub', function () {
-    util.repeatIt('random tests', util.env.repeat, function () {
-      var b32A = util.getMessage()
-      var b32B = util.getTweak()
-      var bnR = BigNum.fromBuffer(b32A).sub(BigNum.fromBuffer(b32B))
+    it('source was not affected', function () {
+      var b32a = util.getMessage()
+      var b32b = util.getTweak()
 
-      var a = BN.fromBuffer(b32A)
-      var b = BN.fromBuffer(b32B)
-      var r = a.sub(b)
+      var a = BN.fromBuffer(b32a)
+      var b = BN.fromBuffer(b32b)
 
-      bnUtil.testBN(r, bnR)
+      bnUtil.testBN(a.sub(b), BigNum.fromBuffer(b32a).sub(BigNum.fromBuffer(b32b)))
+      expect(a.toBuffer().toString('hex')).to.equal(b32a.toString('hex'))
     })
   })
 
@@ -178,7 +486,7 @@ describe('BN', function () {
 
     util.repeatIt('random tests', util.env.repeat, function () {
       var bn = BN.fromBuffer(util.getMessage())
-      if (bn.ucmp(BN.p) >= 0) { bn._redIReduce() }
+      if (bn.ucmp(BN.p) >= 0) { bn.redIReduce() }
 
       bnUtil.testBN(bn.redNeg(), bnUtil.P.sub(BigNum.fromBuffer(bn.toBuffer())))
     })
@@ -219,9 +527,9 @@ describe('BN', function () {
   describe('redMul', function () {
     util.repeatIt('random tests', util.env.repeat, function () {
       var a = BN.fromBuffer(util.getMessage())
-      if (a.ucmp(BN.p) >= 0) { a._redIReduce() }
+      if (a.ucmp(BN.p) >= 0) { a.redIReduce() }
       var b = BN.fromBuffer(util.getTweak())
-      if (b.ucmp(BN.p) >= 0) { b._redIReduce() }
+      if (b.ucmp(BN.p) >= 0) { b.redIReduce() }
 
       var bnA = BigNum.fromBuffer(a.toBuffer())
       var bnB = BigNum.fromBuffer(b.toBuffer())
@@ -233,7 +541,7 @@ describe('BN', function () {
     util.repeatIt('random tests', util.env.repeat, function () {
       var b32 = util.getMessage()
       var a = BN.fromBuffer(b32)
-      if (a.ucmp(BN.p) >= 0) { a._redIReduce() }
+      if (a.ucmp(BN.p) >= 0) { a.redIReduce() }
 
       bnUtil.testBN(a.redInvm(), BigNum.fromBuffer(a.toBuffer()).invertm(bnUtil.P))
     })
