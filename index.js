d5ffe9668513d0a7ac0ef0619a8c3b0fe4ec9c3f
Kirill Fomichev <fanatid@ya.ru>
2017-06-07 16:39:19 +0300

    Show warning only if DEBUG set

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 try {
   module.exports = require('./bindings')
 } catch (err) {
-  console.log('Secp256k1 bindings are not compiled. Pure JS implementation will be used.')
+  if (process.env.DEBUG) {
+    console.error('Secp256k1 bindings are not compiled. Pure JS implementation will be used.')
+  }
+
   module.exports = require('./elliptic')
 }
